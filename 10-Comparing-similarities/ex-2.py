fdc6c13657d9915445de4747d6c2e8ffe3cdb0f2
ehom <ehom@users.noreply.github.com>
2019-10-05 09:59:44 -0700

    Create ex-2.py

---
+++ 10-Comparing-similarities/ex-2.py
@@ -0,0 +1,12 @@
+import spacy
+
+# Use the token.similarity method to compare token1 to token2 and print the result.
+
+nlp = spacy.load("en_core_web_md")
+
+doc = nlp("TV and books")
+token1, token2 = doc[0], doc[2]
+
+# Get the similarity of the tokens "TV" and "books"
+similarity = token1.similarity(token2)
+print(similarity)
